20d4bb598a4b8e37f1f65dca254703156060439b
GusiPusi123 <matveykulmanov0809.1219@gmail.com>
2026-05-27 11:57:19 +0300

    Update 14

--- UpHook/Assets/Player/Scripts/Components/WallClimb.cs
+++ UpHook/Assets/Player/Scripts/Components/WallClimb.cs
@@ -1,3 +1,68 @@
+// using UnityEngine;
+
+// public class WallClimb : MonoBehaviour
+// {
+//     public float climbSpeed = 3f;
+//     public float detectionDistance = 1f;
+//     public LayerMask climbableLayer;
+//     public Animator animator; // Добавляем компонент Animator
+
+//     private Rigidbody rb;
+//     private bool isAttached = false;
+
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody>();
+//     }
+
+//     void Update()
+//     {
+//         // Постоянная проверка касания стены
+//         bool touchingWall = Physics.Raycast(transform.position, transform.forward, detectionDistance, climbableLayer);
+
+//         if (touchingWall)
+//         {
+//             // Если нажата клавиша E, прикрепиться
+//             if (Input.GetKeyDown(KeyCode.E))
+//             {
+//                 isAttached = true;
+//                 rb.useGravity = false;
+
+//                 // Включить анимацию залаза
+//                 if (animator != null)
+//                 {
+//                     animator.SetTrigger("ClimbStart");
+//                 }
+//             }
+//         }
+//         else
+//         {
+//             // Если не касается стены, отключить лазание и включить гравитацию
+//             if (isAttached)
+//             {
+//                 isAttached = false;
+//                 rb.useGravity = true;
+//             }
+//         }
+
+//         if (isAttached)
+//         {
+//             // Управление лазанием
+//             float vertical = Input.GetAxis("Vertical");
+//             float horizontal = Input.GetAxis("Horizontal");
+//             Vector3 moveDirection = (transform.up * vertical + transform.right * horizontal).normalized;
+//             transform.position += moveDirection * climbSpeed * Time.deltaTime;
+
+//             // Отсоединение по F
+//             if (Input.GetKeyDown(KeyCode.F))
+//             {
+//                 isAttached = false;
+//                 rb.useGravity = true;
+//             }
+//         }
+//     }
+// }
+
 using UnityEngine;
 
 public class WallClimb : MonoBehaviour
@@ -5,6 +70,7 @@ public class WallClimb : MonoBehaviour
     public float climbSpeed = 3f;
     public float detectionDistance = 1f;
     public LayerMask climbableLayer;
+    public Animator animator; // Компонент Animator
 
     private Rigidbody rb;
     private bool isAttached = false;
@@ -16,25 +82,34 @@ public class WallClimb : MonoBehaviour
 
     void Update()
     {
-        // Постоянная проверка касания стены
+        // Проверка касания стены
         bool touchingWall = Physics.Raycast(transform.position, transform.forward, detectionDistance, climbableLayer);
 
         if (touchingWall)
         {
-            // Если нажата клавиша E, прикрепиться
+            // Начать лазание по нажатию E
             if (Input.GetKeyDown(KeyCode.E))
             {
                 isAttached = true;
                 rb.useGravity = false;
+
+                // Можно инициировать анимацию начала лазания
+                // (если нужно, например, trigger)
             }
         }
         else
         {
-            // Если не касается стены, отключить лазание и включить гравитацию
+            // Отключить лазание, если не касаешься стены
             if (isAttached)
             {
                 isAttached = false;
                 rb.useGravity = true;
+
+                // Отключить анимацию
+                if (animator != null)
+                {
+                    animator.SetBool("IsClimbing", false);
+                }
             }
         }
 
@@ -44,13 +119,29 @@ public class WallClimb : MonoBehaviour
             float vertical = Input.GetAxis("Vertical");
             float horizontal = Input.GetAxis("Horizontal");
             Vector3 moveDirection = (transform.up * vertical + transform.right * horizontal).normalized;
+
+            // Перемещение
             transform.position += moveDirection * climbSpeed * Time.deltaTime;
+
+            // Управление анимацией
+            if (animator != null)
+            {
+                // Включать анимацию, если игрок движется
+                bool isMoving = Mathf.Abs(vertical) > 0.1f || Mathf.Abs(horizontal) > 0.1f;
+                animator.SetBool("IsClimbing", isMoving);
+            }
 
             // Отсоединение по F
             if (Input.GetKeyDown(KeyCode.F))
             {
                 isAttached = false;
                 rb.useGravity = true;
+
+                // Отключить анимацию
+                if (animator != null)
+                {
+                    animator.SetBool("IsClimbing", false);
+                }
             }
         }
     }
